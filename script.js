87d0c111628fba1720c09bb86d10125ec1f49faf
liamhill123 <>
2021-05-31 00:15:03 +0000

    Animated title

--- script.js
+++ script.js
@@ -3,3 +3,15 @@ var overlay = document.getElementById("overlay");
 window.addEventListener('load', function(){
   overlay.style.display = 'none';
 })
+
+
+
+
+var titleText = document.title;
+
+function titleMarquee() {
+
+ titleText = titleText.substring(1, titleText.length) + titleText.substring(0, 1);
+ document.title = titleText;
+ setTimeout("titleMarquee()", 100);
+ }
