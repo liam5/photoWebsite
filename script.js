67fde5213525841434c09a7845c1ce2aa4ca5999
liamhill123 <83625659+liam5@users.noreply.github.com>
2021-06-14 00:03:11 +0000

    removed button

--- script.js
+++ script.js
@@ -5,29 +5,3 @@ window.addEventListener('load', function(){
 })
 
 
-
-
-var titleText = document.title;
-
-function titleMarquee() {
-
- titleText = titleText.substring(1, titleText.length) + titleText.substring(0, 1);
- document.title = titleText;
- setTimeout("titleMarquee()", 100);
- }
-
-
-function myFunction() {
-  /* Get the text field */
-  var copyText = document.getElementById("myInput");
-
-  /* Select the text field */
-  copyText.select();
-  copyText.setSelectionRange(0, 99999); /* For mobile devices */
-
-  /* Copy the text inside the text field */
-  document.execCommand("copy");
-
-  /* Alert the copied text */
-  alert("Copied the text: " + copyText.value);
-}
