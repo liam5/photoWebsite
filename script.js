ec3795e2cf80343129b82331f23b0ce7186b6482
liamhill123 <83625659+liam5@users.noreply.github.com>
2021-06-20 23:55:12 +0000

    About text tempate created

--- script.js
+++ script.js
@@ -5,3 +5,12 @@ window.addEventListener('load', function(){
 })
 
 
+
+var titleText = document.title;
+
+function titleMarquee() {
+
+ titleText = titleText.substring(1, titleText.length) + titleText.substring(0, 1);
+ document.title = titleText;
+ setTimeout("titleMarquee()", 100);
+ }
